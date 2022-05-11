7ef24c1720f1089947a1085e6254c8650c2e7597
andreluft76 <103416930+Lufti76MatrixReloaded@users.noreply.github.com>
2022-05-11 13:22:49 +0200

    Initial commit

---
+++ lektion5/js/aufgaben/lektion5.js
@@ -0,0 +1,41 @@
+/**
+ * Lektion 5: Schleifen
+ */
+
+/**
+ * 1) Schreibe ein Array `artikelListe`, fülle es und geb es auf der Konsole mithilfe einer Schleife aus
+ */
+function artikelAuflisten() {
+    // ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * 2) Erstelle die Arrays `milchListe`, `gemüseListe` und `getreideListe`, fülle diese, füge sie der gruppenListe hinzu und
+ * gebe sie auf der Konsole aus
+ */
+function allesAuflisten() {
+    let gruppenListe = []
+
+// ToDo: füge ab hier deinen Code ein
+}
+
+/**
+ * 3) Erstelle ein befülltes Array `gemueseListe`, entferne diese Elemente aus dem Array und gebe es vor und nach dem Entfernen
+ * auf der Konsole aus
+ */
+function artikelEntfernen() {
+    // ToDo: füge ab hier deinen Code ein
+
+}
+
+/**
+ * 4) Schreibe die Sortieren-Funktion
+ */
+function sortieren() {
+    // ToDo: füge ab hier deinen Code ein
+}
+
+export {
+    artikelAuflisten, allesAuflisten, artikelEntfernen, sortieren
+}
